9e5bcc0bcbbeaeecb3de913cf6ff60d8b1060435
NguyenToan3107 <nguyentoan31072k3@gmail.com>
2024-02-02 22:44:27 +0700

    Finished create repositories

---
+++ ShopApp_Backend/shopapp/src/main/java/com/project/shopapp/repositories/OrderDetailRepository.java
@@ -0,0 +1,12 @@
+package com.project.shopapp.repositories;
+
+import com.project.shopapp.models.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+//    @Query("SELECT od FROM OrderDetail od WHERE od.order.id = ?1")
+    List<OrderDetail> findByOrderId(Long orderId);
+}
